1f50eb188958336da1ae574be4347b3525c8ef61
Gleb Goncharov <avvogg55@gmail.com>
2023-04-24 18:58:24 +0300

    fix: refactor classes and ids according to 3.4 paragraph from html-and-css

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
+    listItem.id="li-all";
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
@@ -42,8 +43,10 @@ var createNewTaskElement=function(taskString){
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
+    editButton.id="butt";
 
     deleteButton.className="delete";
+    deleteButton.id="butt";
     deleteButtonImg.src="./remove.svg";
     deleteButton.appendChild(deleteButtonImg);
 
